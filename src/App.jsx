645e346e0969ef9fa9f1f2ef7e9ee33756db58d4
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2026-03-05 15:36:17 +0530

    Glowix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <div className="flex-grow">
+      <div className="grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
