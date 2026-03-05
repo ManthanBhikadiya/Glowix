d0b4d29eb2d351990741ed0609acbe834c2d7c71
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2026-03-05 15:46:24 +0530

    Glowix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Services from "./pages/services";
+import Services from "./pages/Services";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Appointment from "./pages/Appointment";
